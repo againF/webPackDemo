6129d5edc30102439c6a5c1bd68569f5c1ad2732
ptdev <ptdev@salarycn.net>
2017-12-26 09:26:40 +0800

    add picture

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import _ from 'lodash';
+import './style.css';
+import Penguins from './Penguins.jpg';
 
 function component() {
     var element = document.createElement('div');
   
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
+    element.classList.add('hello');
+    var myPenguins = new Image();
+    myPenguins.src = Penguins;
+    element.appendChild(myPenguins);
     return element;
   }
   
