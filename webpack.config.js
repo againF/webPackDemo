1a0392e93f2e4646885adf2fa7f1a4bb8fca002c
againF <wyffcld@163.com>
2017-12-30 12:01:11 +0800

    use webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,9 @@ module.exports = {
         print: './src/print.js'
     },
     devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
     plugins: [
         new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
